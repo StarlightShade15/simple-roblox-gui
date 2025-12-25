6541f3fb413e152a2dd7e1b3cc6d49e54be326c0
freakyfdp-py <bypassfdp1501@gmail.com>
2025-12-25 12:36:04 +0100

    Modify UI panel dimensions and text properties
    Adjust panel positioning and text scaling in UI.

--- ui.lua
+++ ui.lua
@@ -53,8 +53,9 @@ mod.init = function(title, blur)
 	screenGui.Name = title
 	screenGui.Parent = playerGui
 
-	local panel = rect(175 / 2, 325 / 2, 175, 325, 0, screenGui)
-	text(title, 0, 0, 36, 12, nil, 16, Color3.fromRGB(255, 255, 255), panel, true)
+	local panel = rect(0, 0, 175, 325, 0, screenGui)
+	text(title, 0, 10, 175, 30, nil, 16, Color3.fromRGB(255, 255, 255), panel, true).TextScaled = true
+	
 	return screenGui
 end
 
